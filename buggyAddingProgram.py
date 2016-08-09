306918164268fca20253753cbd2c5997b9d7cc1c
Joshua Miller <joshua.miller@fitsnips.net>
2016-08-09 02:41:38 -0700

    new file buggyAddingProgram.py for doing step debugging

---
+++ buggyAddingProgram.py
@@ -0,0 +1,9 @@
+#! python3
+
+print('Enter the first nubmer to add: ')
+first = input()
+print('Enter the second numbe to add: ')
+second = input()
+print('Enter a third number to add: ')
+third = input()
+print('The sum is ' + first + second + third)
