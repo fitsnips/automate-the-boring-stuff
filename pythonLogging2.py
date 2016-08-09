d43db04c76f1f2acb2d60442f379a06eceeeb2db
Joshua Miller <joshua.miller@fitsnips.net>
2016-08-09 02:27:34 -0700

    shutdown the logging facility

--- pythonLogging2.py
+++ pythonLogging2.py
@@ -8,3 +8,7 @@ logging.info('The logging module is working.')
 logging.warning('An error message is about to be logged')
 logging.error('An error has occured')
 logging.critical('The program is unable to recover!')
+
+
+# we must close down cleanly
+logging.shutdown()
