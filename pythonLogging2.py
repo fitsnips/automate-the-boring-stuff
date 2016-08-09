f1586701a013d1aace86e1b16af94b2c6b340a15
Joshua Miller <joshua.miller@fitsnips.net>
2016-08-09 02:24:13 -0700

    Testing logging levels in logging module

---
+++ pythonLogging2.py
@@ -0,0 +1,10 @@
+#! python3
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Some debugging details')
+logging.info('The logging module is working.')
+logging.warning('An error message is about to be logged')
+logging.error('An error has occured')
+logging.critical('The program is unable to recover!')
